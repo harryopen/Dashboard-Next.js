0d66f82b8d612a1e4c6e680946d76f21ad116a0a
AS-MAC-1497 <as-mac-1497@AS-MAC-1497s-MacBook-Air.local>
2026-05-24 23:10:41 +0530

    fix: transpile nextjs-auth0 to solve esbuild parsing error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['@auth0/nextjs-auth0'],
 };
 
 export default nextConfig;
