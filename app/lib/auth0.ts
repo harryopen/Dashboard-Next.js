eaa3cf1860be220e77a38f10286305bfda463f3f
AS-MAC-1497 <as-mac-1497@AS-MAC-1497s-MacBook-Air.local>
2026-05-26 19:18:00 +0530

    changes

--- app/lib/auth0.ts
+++ app/lib/auth0.ts
@@ -1,5 +1,3 @@
-import { Auth0Client } from '@auth0/nextjs-auth0/server';
+import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
-export const auth0 = new Auth0Client({
-  appBaseUrl: process.env.APP_BASE_URL,
-});
+export const auth0 = new Auth0Client();
